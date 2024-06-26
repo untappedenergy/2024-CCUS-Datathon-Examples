f77f3f21b1bd4b2e65ab7889596d275575fba99e
untappedenergy <54632037+untappedenergy@users.noreply.github.com>
2024-06-26 14:49:14 -0600

    init commit

---
+++ datathon_examples/R-analytics/wordcloud.R
@@ -0,0 +1,9 @@
+install.packages('tidyverse')
+install.packages('ggplot2')
+install.packages('dplyr')
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+
+ <- read_csv(file = 'tweet_data.csv')
